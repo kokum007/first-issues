9ad086e32b84c4e2e27331d5199f50604bec04c9
Sunali Rambukwella <44220456+sunalii@users.noreply.github.com>
2020-10-05 21:50:43 +0530

    Java issues

---
+++ Java/whileloop.java
@@ -0,0 +1,17 @@
+// Java program to demostrate the while loop. 
+  
+class whileLoopDemo { 
+    public static void main(String args[]) 
+    { 
+        //expression initialization
+        int i = 1; 
+  
+        //testing the expression 
+        while (i < 6) { 
+            System.out.println("This will print 5 times!!!"); 
+  
+            //adding the expression to repeat 
+            i++; 
+        } 
+    } 
+}
